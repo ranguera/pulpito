ca464a9fdc165b01126193b3172eda0bde4c71bc
Roger <ranguera@gmail.com>
2020-02-11 12:14:51 +0100

    init

---
+++ Assets/Script/TentacleBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleBuilder : MonoBehaviour
+{
+    public Transform body;
+    public TentacleManager tentacleManager;
+    public GameObject tentacle_link;
+    public int num_tentacles = 8;
+    public int num_links = 32;
+    public float link_size_decrement = .95f; // 95 %
+    
+    private int i;
+    private GameObject link;
+    private GameObject previous_link;
+
+    void Start()
+    {
+        int angle = 360 / num_tentacles;
+        tentacleManager.Init(num_tentacles);
+
+        for (i = 0; i < num_tentacles; i++)
+        {
+            previous_link = Instantiate(tentacle_link, transform.forward *.6f, transform.rotation, body);
+            tentacleManager.AddLink(i, previous_link.transform);
+            CreateTentacle();
+            transform.Rotate(Vector3.up, angle);
+        }
+    }
+
+    private void CreateTentacle()
+    {
+        for (int j = 0; j < num_links; j++)
+        {
+            link = Instantiate(tentacle_link, previous_link.transform.position + (previous_link.transform.forward*.2f*link_size_decrement), previous_link.transform.rotation, body);
+            link.transform.localScale = previous_link.transform.localScale * link_size_decrement;
+            previous_link = link;
+            tentacleManager.AddLink(i, link.transform);
+        }
+    }
+}
---
+++ Assets/Script/TentacleManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleManager : MonoBehaviour
+{
+    public Transform tentacle_base;
+    public GameObject head;
+    [ColorUsage(true, true)]
+    public Color off;
+    [ColorUsage(true, true)]
+    public Color on;
+
+    private List<Tentacle> tentacles;
+    private Vector3 temp;
+    
+    public void Init(int num_tentacles)
+    {
+        tentacles = new List<Tentacle>();
+        for (int i = 0; i < num_tentacles; i++)
+        {
+            tentacles.Add(new Tentacle());
+            tentacles[i].tentacle = new List<Transform>();
+        }
+
+        InvokeRepeating("TriggerPulse", 3f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < tentacles.Count; i++)
+        {
+            for (int j = 0; j < tentacles[i].tentacle.Count; j++)
+            {
+                float mov = (j/10f)*Mathf.PerlinNoise((Time.time+j)*.1f, i*10f) - .5f;
+                temp = tentacles[i].tentacle[j].position;
+                temp.y = tentacle_base.transform.position.y + mov;
+                tentacles[i].tentacle[j].position = temp;
+            }
+        }
+    }
+
+    public void AddLink(int index, Transform t)
+    {
+        tentacles[index].tentacle.Add(t);
+    }
+
+    private void TriggerPulse()
+    {
+        StartCoroutine(LightUp());
+        StartCoroutine(LightBody());
+    }
+
+    private IEnumerator LightUp()
+    {
+        for (int j = 0; j < tentacles[0].tentacle.Count; j++)
+        {
+            for (int i = 0; i < tentacles.Count; i++)
+            {
+                tentacles[i].tentacle[j].gameObject.GetComponent<Renderer>().material.color = on;
+            }
+            yield return 0;
+
+            if (j > 0)
+            {
+                for (int i = 0; i < tentacles.Count; i++)
+                {
+                    tentacles[i].tentacle[j-1].gameObject.GetComponent<Renderer>().material.color = off;
+                }
+                yield return 0;
+            }
+        }
+
+        for (int i = 0; i < tentacles.Count; i++)
+        {
+            tentacles[i].tentacle[tentacles[i].tentacle.Count - 1].gameObject.GetComponent<Renderer>().material.color = off;
+        }
+    }
+
+    private IEnumerator LightBody()
+    {
+        float h, s, v;
+        Color.RGBToHSV(on, out h, out s, out v);
+
+        GetComponent<Renderer>().material.color = on;
+        head.GetComponent<Renderer>().material.color = on;
+
+        for (int i = 0; i < 50; i++)
+        {
+            v -= i/50f;
+            GetComponent<Renderer>().material.color = Color.HSVToRGB(h, s, v, true);
+            head.GetComponent<Renderer>().material.color = Color.HSVToRGB(h, s, v, true);
+            yield return null;
+        }
+        
+    }
+}
